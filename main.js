03085d2dd77eef824441c21d1693b642e8909593
lavalliu <146725126+lavalliu@users.noreply.github.com>
2024-06-04 18:53:27 +0400

    testing another code

--- main.js
+++ main.js
@@ -5,21 +5,27 @@ const app=express();
 app.use(bodyParser.json());  // parse requests of content-type - application/json
 app.use(bodyParser.urlencoded({ extended: true }));  // parse requests of content-type - application/x-www-form-urlencoded
 
-app.set('view engine', 'ejs');
-
-// const path = require('path');
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
-const path = require('path');
-app.set('views', path.join('/', 'views'));
-
 //index page
 app.get('/', (req,res) => {
-    res.render('index');
+    res.render('index.ejs');
 });
 
-const port = 3001;
-app.listen(port, '0.0.0.0', () => {
-console.log(`server listening on ${port}`);
+//login page
+app.get('/login', (req,res) => {
+    res.render('login.ejs');
+});
+
+//register page
+app.get('/register', (req,res) => {
+    res.render('register.ejs');
+});
+
+//journal individual page
+app.get('/page', (req,res) => {
+    res.render('page.ejs');
+});
+
+const port=3001;
+app.listen(port, () => {
+    console.log(`server listening on ${port}`);
 });
