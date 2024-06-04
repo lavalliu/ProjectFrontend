67beae5c45a47d211d01172bae78e182b6fccb7d
lavalliu <146725126+lavalliu@users.noreply.github.com>
2024-06-04 16:02:21 +0400

    updated so that it runs on render

---
+++ main.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const app=express();
+app.use(bodyParser.json());  // parse requests of content-type - application/json
+app.use(bodyParser.urlencoded({ extended: true }));  // parse requests of content-type - application/x-www-form-urlencoded
+
+//index page
+app.get('/', (req,res) => {
+    res.render('/PUBLIC/index.ejs');
+});
+
+const port=3001;
+app.listen(port, () => {
+    console.log(`server listening on ${port}`);
+});
