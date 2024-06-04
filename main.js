7da0b37f49733162eb92b427b1c3b53b0d0b4acd
lavalliu <146725126+lavalliu@users.noreply.github.com>
2024-06-04 16:41:55 +0400

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,14 @@ const app=express();
 app.use(bodyParser.json());  // parse requests of content-type - application/json
 app.use(bodyParser.urlencoded({ extended: true }));  // parse requests of content-type - application/x-www-form-urlencoded
 
+
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 //index page
 app.get('/', (req,res) => {
-    res.render('/index.ejs');
+    res.render('index.ejs');
 });
 
 const port=3001;
