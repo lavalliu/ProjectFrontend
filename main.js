e21412a9e4e28674b5c2ca87b3be21ecf440ed75
lavalliu <146725126+lavalliu@users.noreply.github.com>
2024-06-05 22:11:08 +0400

    added all pages to route in main,js

--- main.js
+++ main.js
@@ -12,10 +12,102 @@ app.set('view engine', 'ejs');
 // app.set('view engine', 'ejs');
 
 const path = require('path');
-app.set('views', path.join('/', 'views'));
+app.set('templates', path.join('/', 'templates'));
 
 //index page
 app.get('/', (req,res) => {
+    res.render('index');
+});
+
+app.get('/about', (req,res) => {
+    res.render('index');
+});
+
+app.get('/additems', (req,res) => {
+    res.render('index');
+});
+
+app.get('/aperitifs', (req,res) => {
+    res.render('index');
+});
+
+app.get('/cocktails', (req,res) => {
+    res.render('index');
+});
+
+app.get('/contact', (req,res) => {
+    res.render('index');
+});
+
+app.get('/desserts', (req,res) => {
+    res.render('index');
+});
+
+app.get('/displayresa', (req,res) => {
+    res.render('index');
+});
+
+app.get('/hot', (req,res) => {
+    res.render('index');
+});
+
+app.get('/listresas', (req,res) => {
+    res.render('index');
+});
+
+app.get('/login', (req,res) => {
+    res.render('index');
+});
+
+app.get('/maincourse', (req,res) => {
+    res.render('index');
+});
+
+app.get('/modifyitems', (req,res) => {
+    res.render('index');
+});
+
+app.get('/navbar', (req,res) => {
+    res.render('index');
+});
+
+app.get('/order', (req,res) => {
+    res.render('index');
+});
+
+app.get('/page', (req,res) => {
+    res.render('index');
+});
+
+app.get('/payment', (req,res) => {
+    res.render('index');
+});
+
+app.get('/register', (req,res) => {
+    res.render('index');
+});
+
+app.get('/reservation', (req,res) => {
+    res.render('index');
+});
+
+app.get('/soft', (req,res) => {
+    res.render('index');
+});
+
+app.get('/starters', (req,res) => {
+    res.render('index');
+});
+
+app.get('/update', (req,res) => {
+    res.render('index');
+});
+
+app.get('/user', (req,res) => {
+    res.render('index');
+});
+
+app.get('/wine', (req,res) => {
     res.render('index');
 });
 
