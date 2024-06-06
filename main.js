33df47f98fe730b76fed2e4a7dc3d48ffe190436
lavalliu <146725126+lavalliu@users.noreply.github.com>
2024-06-06 09:41:37 +0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.json()); // parse requests of content-type - application/json
-app.use(bodyParser.urlencoded({ extended: true })); // parse requests of content-type - application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true })); // parse requests of content-type - application/x-www-form-urlencoded
 
 app.use(express.static('/public'));
 
